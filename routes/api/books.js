d17ca7d911d8383357a36fbc9091cf43087e8097
Jake Scudder <scudsterr@gmail.com>
2020-10-27 16:46:29 -0400

    debug add book cover is a promise

--- routes/api/books.js
+++ routes/api/books.js
@@ -25,38 +25,67 @@ router.get("/", (req, res) => {
 // @access Public
 router.post("/", (req, res) => {
   console.log("post request body:", req.body);
-  req.setTimeout(70000);
+  // req.setTimeout(70000);
   let maxImage;
-  //Fetches higher quality image
-  bookcovers.withIsbn(req.body.isbn).then((res) => {
-    console.log("getting book cover");
-    if (res.amazon["3x"]) {
-      maxImage = res.amazon["3x"];
-    } else if (res.amazon["2.5x"]) {
-      maxImage = res.amazon["2.5x"];
-    } else if (res.amazon["2x"]) {
-      maxImage = res.amazon["2x"];
-    }
-  })
-  //debug heroku
-  .then(() => {
-    console.log("now adding book to db")
-    const newBook = new Book({
-      title: req.body.title,
-      subtitle: req.body.subtitle,
-      author: req.body.author,
-      publishedDate: req.body.publishedDate,
-      description: req.body.description,
-      image: maxImage,
-      category: req.body.category,
-    });
 
-    newBook
-      .save()
-      .then((book) => res.json(book))
+  //Fetches higher quality image with Promise
+  const promise = new Promise((resolve, reject) => {
+    bookcovers.withIsbn(req.body.isbn).then((res) => {
+      console.log("getting book cover response:", res);
+      if (res.amazon["3x"]) {
+        maxImage = res.amazon["3x"];
+        resolve(res)
+      } 
+      if (res.amazon["2.5x"]) {
+        maxImage = res.amazon["2.5x"];
+        resolve(res)
+      } 
+      if (res.amazon["2x"]) {
+        maxImage = res.amazon["2x"];
+        resolve(res)
+      } else {
+        const error = { success: false }
+        reject(error)
+      }
+    })
   })
 
-  //First iteration of addBook
+  //Will not build book until promise resolves
+  promise
+    //debug heroku
+    .then(() => {
+      console.log("now adding book to db")
+      const newBook = new Book({
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        author: req.body.author,
+        publishedDate: req.body.publishedDate,
+        description: req.body.description,
+        image: maxImage,
+        category: req.body.category,
+      });
+
+      newBook
+        .save()
+        .then((book) => res.json(book))
+    })
+    .catch(err => {
+      res.status(500).send({success: false}, "Failed to add book:", err);
+    })
+});
+
+// @route DELETE api/books/:id
+// @desc Delete a Book
+// @access Public
+router.delete("/:id", (req, res) => {
+  Book.findById(req.params.id)
+    .then((book) => book.remove().then(() => res.json({ success: true })))
+    .catch((err) => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
+
+//First iteration of addBook
   // //Once max-image is found, add entire book to database
   // let checkExist = setInterval(() => {
   //   if (maxImage) {
@@ -82,43 +111,3 @@ router.post("/", (req, res) => {
   //       );
   //   }
   // }, 1000);
-});
-
-// @route DELETE api/books/:id
-// @desc Delete a Book
-// @access Public
-router.delete("/:id", (req, res) => {
-  Book.findById(req.params.id)
-    .then((book) => book.remove().then(() => res.json({ success: true })))
-    .catch((err) => res.status(404).json({ success: false }));
-});
-
-module.exports = router;
-
-//*****Second Iteration
-// setTimeout(() => {
-//   const newBook = new Book({
-//     title: req.body.title,
-//     subtitle: req.body.subtitle,
-//     author: req.body.author,
-//     publishedDate: req.body.publishedDate,
-//     description: req.body.description,
-//     image: maxImage,
-//     category: req.body.category,
-//   });
-
-//   newBook.save().then((book) => res.json(book));
-// }, 15000);
-
-//*****First iteration
-// const newBook = new Book({
-//   title: req.body.title,
-//   subtitle: req.body.subtitle,
-//   author: req.body.author,
-//   publishedDate: req.body.publishedDate,
-//   description: req.body.description,
-//   image: maxImage,
-//   category: req.body.category,
-// });
-
-// newBook.save().then((book) => res.json(book));
